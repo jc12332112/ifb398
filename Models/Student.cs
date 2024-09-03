b92e2b8788af49cbd962a54b24ca790e2a3a705c
JC123321 <chengjiayiwa14@gmail.com>
2024-09-03 17:46:50 +1000

    添加项目文件。

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+
+    }
+    
+        
+}
